816c9c768ee02ce5914371a9594297bd1ad10420
SimonsMeldzers <56105687+SimonsMeldzers@users.noreply.github.com>
2023-04-14 16:55:08 +0000

    test

--- hash_substring.py
+++ hash_substring.py
@@ -1,29 +1,52 @@
 # python3
 
 def read_input():
-    # this function needs to aquire input both from keyboard and file
-    # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    
-    
-    # after input type choice
-    # read two lines 
-    # first line is pattern 
-    # second line is text in which to look for pattern 
-    
-    # return both lines in one return
-    
-    # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
+        file_type = input().strip()
+        if file_type == 'F':
+            file_name = input()
+
+            if 'a' in file_name:
+                return
+            if 'tests/' not in file_name:
+                file_name = 'tests/' + file_name
+            with open(file_name) as f:
+                pattern = f.readline().strip()
+                text = input().strip()
+
+        elif file_type == 'I':
+            pattern = f.readline().strip()
+            text = input().strip()
+        else:
+            return 'Error'
+
+        return pattern, text
 
 def print_occurrences(output):
-    # this function should control output, it doesn't need any return
     print(' '.join(map(str, output)))
 
 def get_occurrences(pattern, text):
-    # this function should find the occurances using Rabin Karp alghoritm 
+    p = 31
+    m = 10**9 + 9
+    n = len(pattern)
+    t = len(text)
+    
+    text_hash = 0
+    for i in range(n):  
+        text_hash = (text_hash * p + ord(text[i])) % m
 
-    # and return an iterable variable
-    return [0]
+    pattern_hash = 0
+    for i in range(n):
+        pattern_hash = (pattern_hash * p + ord(text[i])) % m
+
+    occurrences = []
+    if text_hash == pattern_hash:
+        occurrences.append()
+
+    for i in range(t-n+1):
+        text_hash = ((text_hash - ord(text[i])) % m) * (p + ord(text(i+n)) % m)
+    
+    return occurrences
+
 
 
 # this part launches the functions
